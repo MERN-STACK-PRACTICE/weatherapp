dbbf4d86baa00db469b301fd070a46c575122117
Lathika Sunder <lathikasunder11@gmail.com>
2023-09-16 17:21:33 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import WeatherComp from "./WeatherComp/WeatherComp"
+
+function App() {
+
+  return (
+<React.Fragment>
+<WeatherComp/>
+</React.Fragment>
+  )
+}
+
+export default App
